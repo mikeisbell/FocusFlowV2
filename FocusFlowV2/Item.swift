73efb0f4f152efa04cb7bb54117248de9eac5a40
Mike Isbell <misbell@live.com>
2026-04-21 21:09:31 -0700

    Initial Commit

---
+++ FocusFlowV2/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FocusFlowV2
+//
+//  Created by Michael Isbell on 4/21/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ FocusFlowV2Tests/FocusFlowV2Tests.swift
@@ -0,0 +1,19 @@
+//
+//  FocusFlowV2Tests.swift
+//  FocusFlowV2Tests
+//
+//  Created by Michael Isbell on 4/21/26.
+//
+
+import Testing
+@testable import FocusFlowV2
+
+struct FocusFlowV2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
